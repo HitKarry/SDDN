5f72add53be0a84120dbac623f4eadbc79e59d9c
HitKarry <karry@stu.hit.edu.cn>
2024-06-11 21:04:38 +0800

    Add files via upload

---
+++ ESPCN/espcn.py
@@ -0,0 +1,21 @@
+from torch import nn
+from config import configs
+import torch.nn.functional as F
+import torch
+
+class ESPCN(nn.Module):
+    def __init__(self, upscale_factor):
+        super(ESPCN, self).__init__()
+
+        self.conv1 = nn.Conv2d(3, 64, (5, 5), (1, 1), (2, 2))
+        self.conv2 = nn.Conv2d(64, 64, (3, 3), (1, 1), (1, 1))
+        self.conv3 = nn.Conv2d(64, 32, (3, 3), (1, 1), (1, 1))
+        self.conv4 = nn.Conv2d(32, 3 * (upscale_factor ** 2), (3, 3), (1, 1), (1, 1))
+        self.pixel_shuffle = nn.PixelShuffle(upscale_factor)
+
+    def forward(self, x):
+        x = torch.tanh(self.conv1(x))
+        x = torch.tanh(self.conv2(x))
+        x = torch.tanh(self.conv3(x))
+        x = torch.sigmoid(self.pixel_shuffle(self.conv4(x)))
+        return x
